68106c013cb5c713ede1e32b4a322e6453e1ce46
Killian Stacey <killianstacey5@gmail.com>
2021-01-04 13:45:22 -0800

    statistics: add necessary behindthescenes functions

--- dibs/statistics.py
+++ dibs/statistics.py
@@ -42,6 +42,60 @@ def first_arg(*args):
     return args[0]
 
 
+def convert_int_from_string_if_possible(s: str):
+    """ Converts digit string to integer if possible """
+    if s.isdigit():
+        return int(s)
+    else:
+        return s
+
+
+def alphanum_key(s) -> List:
+    """
+    Turn a string into a list of string and number chunks.
+        e.g.: input: "z23a" -> output: ["z", 23, "a"]
+    """
+    return [convert_int_from_string_if_possible(c) for c in re.split('([0-9]+)', s)]
+
+
+def sort_list_nicely_in_place(list_input: list) -> None:
+    """ Sort the given list (in place) in the way that humans expect. """
+    if not isinstance(list_input, list):
+        raise TypeError(f'argument `l` expected to be of type list but '
+                        f'instead found: {type(list_input)} (value: {list_input}).')
+    list_input.sort(key=alphanum_key)
+
+
+def augmented_runlength_encoding(labels: Union[List, np.ndarray]) -> Tuple[List[Any], List[int], List[int]]:
+    """
+    TODO: med: purpose // purpose unclear
+    :param labels: (list or np.ndarray) predicted labels
+    :return
+        label_list: (list) the label number
+        idx: (list) label start index
+        lengths: (list) how long each bout lasted for
+    """
+    label_list, idx_list, lengths_list = [], [], []
+    i = 0
+    while i < len(labels):
+        # 1/3: Record current index
+        idx_list.append(i)
+        # 2/3: Record current label
+        current_label = labels[i]
+        label_list.append(current_label)
+        # Iterate over i while current label and next label are same
+        start_index = i
+        while i < len(labels)-1 and labels[i] == labels[i + 1]:
+            i += 1
+        end_index = i
+        # 3/3: Calculate length of repetitions, then record lengths to list
+        length = end_index - start_index
+        lengths_list.append(length)
+        # Increment and continue
+        i += 1
+    return label_list, idx_list, lengths_list
+
+
 
 
 
