d1d6c7adc0d739cf23226a0c91d073dcb32e8a64
Killian Stacey <killianstacey5@gmail.com>
2021-01-10 08:58:50 -0800

    _init_: Add extra fields for pypi repo reasons + TODOs

--- dibs/__init__.py
+++ dibs/__init__.py
@@ -2,6 +2,11 @@
 TODO: med: Add in description of this package
 """
 
+__version__ = '0.0.1'  # TODO: review this version #
+__author__ = 'Howland Lab'  # TODO: review author(s)
+__credits__ = 'John Howland Lab at University of Saskatchewan'
+
+
 # General imports for packages
 from . import (
     app,
