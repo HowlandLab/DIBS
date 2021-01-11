3b00cfd586290130132dc579b0aef091bb64befb
Killian Stacey <killianstacey5@gmail.com>
2021-01-11 13:15:23 -0800

    test_PipelineMimic: Add more tests for different TSNE implementations

--- tests/test_PipelineMimic.py
+++ tests/test_PipelineMimic.py
@@ -14,7 +14,7 @@ assert os.path.isfile(csv__train_data__file_path)
 
 class TestPipelineMimic(TestCase):
 
-    def test___CanItEvenBuild(self):
+    def test__build__shouldRunFine__whenUsingDefaults(self):
         # Arrange
         gmm_n_components, cv = 2, 3  # Set gmm clusters low so that it can still work with 10 rows of data
         p = dibs.pipeline.PipelineMimic(f'TestPipeline_{random.randint(0, 100_000_000)}',
@@ -34,6 +34,48 @@ class TestPipelineMimic(TestCase):
 
 
 """
+        self.assertTrue(True)
+
+    def test__build__shouldBuildFine__whenBhtsneIsSpecified(self):
+        # Arrange
+        gmm_n_components, cv = 2, 3  # Set gmm clusters low so that it can still work with 10 rows of data
+        p = dibs.pipeline.PipelineMimic(f'TestPipeline_{random.randint(0, 100_000_000)}',
+                                        cross_validation_k=cv,
+                                        gmm_n_components=gmm_n_components,
+
+                                        )
+        err = f"""Sanity Check: Something bad happened and cross val is not right"""
+        self.assertEqual(cv, p.cross_validation_k, err)
+        p = p.add_train_data_source(csv__train_data__file_path)
+        p.tsne_implementation = 'bhtsne'
+        # Act
+
+        p = p.build()
+
+        # Assert
+        err = f"""
+
+
+
+"""
+        self.assertTrue(True)
+
+    def test__build__shouldBuildFine__whenOpentsneIsSpecified(self):
+        # Arrange
+        gmm_n_components, cv = 2, 3  # Set gmm clusters low so that runtime isn't long
+        p = dibs.pipeline.PipelineMimic(f'TestPipeline_{random.randint(0, 100_000_000)}',
+                                        cross_validation_k=cv,
+                                        gmm_n_components=gmm_n_components,
+                                        )
+        err = f"""Sanity Check: Something bad happened and cross val is not right"""
+        self.assertEqual(cv, p.cross_validation_k, err)
+        p = p.add_train_data_source(csv__train_data__file_path)
+        p.tsne_implementation = 'opentsne'
+        # Act
+
+        p = p.build()
+
+        # Assert
         self.assertTrue(True)
 
 
