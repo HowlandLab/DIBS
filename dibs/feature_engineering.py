8b6595f870af4d3357c439e151c612e0b7e71820
Aaron Toderash (adt150) <aaron.t@usask.ca>
2021-01-18 22:00:35 -0600

    Aaron: feature_engingeering: helper functions for calculating angles between vectors

--- dibs/feature_engineering.py
+++ dibs/feature_engineering.py
@@ -36,6 +36,23 @@ from dibs import check_arg, config, logging_enhanced, statistics
 
 logger = config.initialize_logger(__name__)
 
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 ### Attach features as columns to a DataFrame of DLC data
 
