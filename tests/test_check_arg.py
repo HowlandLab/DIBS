0b028d374b760d5055e335a73484ae4a3a1c90b6
Killian Stacey <killianstacey5@gmail.com>
2021-02-24 14:36:26 -0800

    test_check_arg: add TSNE perplexity lambda tests

--- tests/test_check_arg.py
+++ tests/test_check_arg.py
@@ -14,7 +14,7 @@ single_test_file_location = dibs.config.TEST_FILE__PipelineMimic__CSV__TRAIN_DAT
 
 class TestCheckArg(TestCase):
 
-    # ensure_numpy_arrays_are_same_shape
+    # Ensure_numpy_arrays_are_same_shape
     def test__ensure_numpy_arrays_are_same_shape__shouldRunWithoutError__whenArraysAreSameShape(self):
         data1 = [[1, 2, 3], [1, 2, 3]]
         data2 = [[1, 2, 3], [1, 2, 3]]
@@ -110,6 +110,33 @@ class TestCheckArg(TestCase):
         expected_type = (float, str)
         expected_error = TypeError
         self.assertRaises(expected_error, dibs.check_arg.ensure_type, integer_var, *expected_type)
+
+    # Check valid TSNE perplexity lambda
+    def test__ensure_valid_perplexity_lambda__shouldRunWithoutError__whenGivenAValidLambda(self):
+        # Arrange
+        valid_lambda_str = 'lambda self: 42'
+        try:
+            dibs.check_arg.ensure_valid_perplexity_lambda(valid_lambda_str)
+        except Exception as e:
+            raise e
+
+    def test__ensure_valid_perplexity_lambda__shouldProduceError__whenLambdaIsNotActuallALambda(self):
+        # Arrange
+        invalid_lambda_str = 'def x(): return 1'
+        expected_error = ValueError
+        self.assertRaises(expected_error, dibs.check_arg.ensure_valid_perplexity_lambda, invalid_lambda_str)
+
+    def test__ensure_valid_perplexity_lambda__shouldProduceError__whenLambdaStrMissingSelfButHasReplacement(self):
+        # Arrange
+        invalid_lambda_str = 'lambda x, *args: 123'
+        expected_error = ValueError
+        self.assertRaises(expected_error, dibs.check_arg.ensure_valid_perplexity_lambda, invalid_lambda_str)
+
+    def test__ensure_valid_perplexity_lambda__shouldProduceError__whenLambdaStrMissingSelf(self):
+        # Arrange
+        invalid_lambda_str = 'lambda *args: 123'
+        expected_error = ValueError
+        self.assertRaises(expected_error, dibs.check_arg.ensure_valid_perplexity_lambda, invalid_lambda_str)
 
     def test__(self):
         # Arrange
