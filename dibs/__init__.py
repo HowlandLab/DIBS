770880c46a9e474bdc59cda5902b83495d395432
Killian Stacey <killianstacey5@gmail.com>
2021-03-04 10:53:38 -0800

    _init_: add example of imports from different package (commented)

--- dibs/__init__.py
+++ dibs/__init__.py
@@ -29,3 +29,17 @@ from .io import (
     read_csv,
     read_pipeline,
 )
+
+## Edgar package exsample of autoimporting modules
+# from os.path import dirname, basename, isfile
+# import glob
+# from .edgar import Edgar
+# from .txtml import TXTML
+# from .company import Company
+# from .xbrl import XBRL, XBRLElement
+# from .document import Document, Documents
+#
+# __version__ = "5.4.1"
+#
+# modules = glob.glob(dirname(__file__)+"/*.py")
+# __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
