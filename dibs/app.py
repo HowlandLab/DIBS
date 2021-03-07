088cf3722cf2b17e4499654e616e2d18b76f36d2
Killian Stacey <killianstacey5@gmail.com>
2021-03-06 16:40:50 -0800

    change pipe naming and timing

--- dibs/app.py
+++ dibs/app.py
@@ -60,11 +60,14 @@ def streamlit(**kwargs) -> None:
 
 def tsnegridsearch():
     # Param section -- MAGIC VARIABLES GO HERE
-    perplexity_fracs = [0.0001, 1e-5, 1e-6, 1e-7, 1e-8, ]
+    perplexity_fracs = [1e-4]
+    perplexity_fracs = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, ]
     # perplexities = list(range(500, 750, 25))
+    exaggerations = [200, ]
     exaggerations = list(range(22, 1100, 22))
+    learn_rates = [100, ]
     learn_rates = [100, 200, 300, 400, 500]
-    percent_epm_train_files_to_cluster_on = 0.75
+    percent_epm_train_files_to_cluster_on = 0.50
     assert 0 < percent_epm_train_files_to_cluster_on <= 1.0
 
 
@@ -111,7 +114,8 @@ def tsnegridsearch():
     start_time = time.perf_counter()
 
     for i, kwargs_i in enumerate(kwargs_product):
-        p_i = pipeline_implementation(pipeline_names_by_index[i], **kwargs_i).add_train_data_source(*(train_data.copy()))
+        results_current_time = time.strftime("%Y-%m-%d_%HH%MM")
+        p_i = pipeline_implementation(f'{pipeline_names_by_index[i]}www_{results_current_time}', **kwargs_i).add_train_data_source(*(train_data.copy()))
         print(f'Start build for pipeline idx {i} -- Frac={p_i._tsne_perplexity}')
         try:
             p_i = p_i.build()
