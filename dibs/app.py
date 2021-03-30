b301834f1d8b8c8a9a39d12d304461f330813738
Killian Stacey <killianstacey5@gmail.com>
2021-03-30 12:34:11 -0700

    app.py: further develop buildone command

--- dibs/app.py
+++ dibs/app.py
@@ -165,105 +165,6 @@ def print_if_system_is_64_bit():
     print(f'This system is detected to be 64-bit: {sys.maxsize > 2**32}')
 
 
-def trybuild(*args, **kwargs):
-    # def make_behaviour_example_videos(self, data_source: str, video_file_path: str, file_name_prefix=None, min_rows_of_behaviour=1, max_examples=3, num_frames_buffer=0, output_fps=15):
-    vid_data_source = 'EPM-MCE-10DLC_resnet50_Maternal_EPMDec28shuffle1_700000'
-    vid_file_path = "C:\\Users\\killian\\projects\\DIBS\\epm-mce-vids-f\\EPM-MCE-10.mp4"
-    assert os.path.isfile(vid_file_path)
-    # Param section -- MAGIC VARIABLES GO HERE
-    max_cores_per_pipe = 8
-    perplexities = [400, 400]
-    exaggerations = [1000]
-    learn_rates = [1000, 1000, 1000, 1000, 1000]
-    tsne_n_iters = [250]
-    percent_epm_train_files_to_cluster_on = 0.5
-
-    num_gmm_clusters_aka_num_colours = 7  # Sets the number of clusters that GMM will try to label
-    pipeline_implementation = pipeline.PipelineHowland  # Another option includes dibs.pipeline.PipelineMimic
-    graph_dimensions = (12, 12)  # length x width.
-    show_cluster_graphs_in_a_popup_window = False  # Set to False to display graphs inline
-    assert 0 < percent_epm_train_files_to_cluster_on <= 1.0
-
-    # Auto-generate the product between all possible parameters
-    kwargs_product = [{
-        'tsne_perplexity': perplexity_i,
-        'tsne_early_exaggeration': early_exaggeration_j,
-        'tsne_learning_rate': learning_rate_k,
-        'gmm_n_components': num_gmm_clusters_aka_num_colours,
-        'tsne_n_components': 2,  # n-D dimensionality reduction
-        'tsne_n_iter': tsne_n_iter,
-        'cross_validation_k': max_cores_per_pipe,
-        'cross_validation_n_jobs': max_cores_per_pipe,
-        'rf_n_jobs': max_cores_per_pipe,
-        'tsne_n_jobs': max_cores_per_pipe,
-    } for learning_rate_k, early_exaggeration_j, perplexity_i, tsne_n_iter in itertools.product(
-        learn_rates,
-        exaggerations,
-        perplexities,
-        # [f'lambda self: self.num_training_data_points * {f}' for f in perplexity_fracs],
-        tsne_n_iters,
-    )]
-    pipeline_names_by_index = [f'Pipeline_{i}' for i in range(len(kwargs_product))]
-    logger.info(f'Number of parameter permutations: {len(kwargs_product)} (starting runtime at: {time.strftime("%Y-%m-%d_%HH%MM")})')
-
-    # Queue up which data files will be added to each Pipeline
-    all_files = [os.path.join(config.DEFAULT_TRAIN_DATA_DIR, file) for file in os.listdir(config.DEFAULT_TRAIN_DATA_DIR)]
-    train_data = all_files[:int(len(all_files) * percent_epm_train_files_to_cluster_on)]
-
-    # print(train_data)  # Uncomment this line to see which exact data files are added to the Pipeline
-
-    # Create list of pipelines with all of the different combinations of parameters inserted
-    # pipelines_ready_for_building = [pipeline_implementation(name, **kwargs).add_train_data_source(*(train_data.copy())) for name, kwargs in zip(pipeline_names_by_index, kwargs_product)]
-
-    # The heavy lifting/processing is done here
-    logger.info(f'# of combinations: {len(pipeline_names_by_index)}')
-    logger.debug(f'Start time: {time.strftime("%Y-%m-%d_%HH%MM")}')
-    successful_builds = 0
-    start_time = time.perf_counter()
-    for i, kwargs_i in enumerate(kwargs_product):
-        start_build = time.perf_counter()
-        results_current_time = time.strftime("%Y-%m-%d_%HH%MM")
-        p_i: pipeline.BasePipeline = pipeline_implementation(f'{pipeline_names_by_index[i]}_{results_current_time}', **kwargs_i).add_train_data_source(*(train_data.copy()))
-        logger.debug(f'Start build for pipeline idx {i} ({i+1} of {len(kwargs_product)})  -- Frac={p_i._tsne_perplexity}')
-        # Try to build
-        try:
-            p_i = p_i.build(skip_accuracy_score=True)
-        except Exception as e:
-            info = f'PerpRaw={p_i._tsne_perplexity}/Perp={p_i.tsne_perplexity}/' \
-                   f'EE={p_i.tsne_early_exaggeration}/LR={p_i.tsne_learning_rate}/GMM-N={p_i.gmm_n_components}'
-            err = f'Unexpected exception::{__name__}.{logging_enhanced.get_current_function()}(): ' \
-                  f'an unexpected exception occurred when building many pipelines to get good graphs. ' \
-                  f'Info is as follows: {info}. Exception is: {repr(e)}.'# Diagnostics: {p_i.diagnostics()}'
-            logger.error(err)
-
-            raise RuntimeError(err)
-        else:
-            # Get info
-            perplexity_ratio_i, perplexity_i, learning_rate_i, early_exaggeration_i = p_i.tsne_perplexity_relative_to_num_data_points, p_i.tsne_perplexity, p_i.tsne_learning_rate, p_i.tsne_early_exaggeration
-
-            graph_title = f"Perp ratio: {round(perplexity_ratio_i, 5)} / " \
-                          f"Perp: {perplexity_i} / EE: {early_exaggeration_i} / " \
-                          f"LearnRate: {learning_rate_i} / #data={p_i.num_training_data_points} / tsneNiter={p_i.tsne_n_iter}"
-            # Save graph to file
-            p_i.plot_clusters_by_assignments(
-                title=graph_title,
-                fig_file_prefix=f'standardscaler1{results_current_time}__{p_i.name}__',
-                show_now=False, save_to_file=True, figsize=graph_dimensions,
-                s=0.4 if show_cluster_graphs_in_a_popup_window else 1.5,
-            )
-            # p_i.save_to_folder()
-            # Try to make example vids
-            # p_i.make_behaviour_example_videos(vid_data_source, vid_file_path, file_name_prefix='timtest4', num_frames_buffer=1)  #     def make_behaviour_example_videos(self, data_source: str, video_file_path: str, file_name_prefix=None, min_rows_of_behaviour=1, max_examples=3, num_frames_buffer=0, output_fps=15):
-
-        end_build = time.perf_counter()
-        logger.info(f'Time to build: {round(end_build-start_build)} seconds (using {max_cores_per_pipe} cores)')
-        successful_builds += 1
-        logger.debug('---------------------------------------------\n\n')
-    end_time = time.perf_counter()
-    logger.info(f'Total compute time: {round((end_time - start_time) / 60, 2)} minutes. Total successful jobs with results: {successful_builds}. Total jobs computed: {len(pipeline_names_by_index)}')
-    logger.debug(f'Done job at: {time.strftime("%Y-%m-%d_%HH%MM")}')
-
-
 def buildone(**kwargs):
     """
     Build just one pipeline, save to file.
@@ -271,23 +172,26 @@ def buildone(**kwargs):
     :param kwargs:
     :return:
     """
-    pipeline_name = kwargs.get('name')
-    if pipeline_name is None:
-        err_type = f'Name was expected to be a string but instead found None (likely due to missing command-line parameter). Try using the -n option.'
-        logger.error(err_type)
-        raise TypeError(err_type)
     # Param section -- MAGIC VARIABLES GO HERE
+    save_to_folder = True
+    save_graph_to_file = True
+    max_cores_per_pipe = 5
+    percent_epm_train_files_to_cluster_on = 1.0
+    pipeline_implementation = pipeline.PipelineHowland  # Another option includes dibs.pipeline.PipelineMimic
+    graph_dimensions = (12, 12)  # length x width?
+    show_cluster_graphs_in_a_popup_window = False  # Set to False to display graphs inline
+
+    pipeline_name = kwargs.get('name')
     # vid_data_source = 'EPM-MCE-10DLC_resnet50_Maternal_EPMDec28shuffle1_700000'
     # vid_file_path = "C:\\Users\\killian\\projects\\DIBS\\epm-mce-vids-f\\EPM-MCE-10.mp4"
     # assert os.path.isfile(vid_file_path), f'Video file not found. Path = {vid_file_path}'
 
-    max_cores_per_pipe = 5
-    percent_epm_train_files_to_cluster_on = 0.12
-    pipeline_implementation = pipeline.PipelineHowland  # Another option includes dibs.pipeline.PipelineMimic
-    graph_dimensions = (12, 12)  # length x width.
-    show_cluster_graphs_in_a_popup_window = False  # Set to False to display graphs inline
+    # Arg checking
     assert 0 < percent_epm_train_files_to_cluster_on <= 1.0
-
+    if pipeline_name is None:
+        err_type = f'Name was expected to be a string but instead found None (likely due to missing command-line parameter). Try using the -n option.'
+        logger.error(err_type)
+        raise TypeError(err_type)
     # Auto-generate the product between all possible parameters
     pipeline_kwargs = {
         'tsne_perplexity': kwargs.get('perplexity', config.TSNE_PERPLEXITY),
@@ -310,40 +214,44 @@ def buildone(**kwargs):
     logger.debug(f'{logging_enhanced.get_current_function()}(): Start time: {time.strftime("%Y-%m-%d_%HH%MM")}')
     start_build = time.perf_counter()
     results_current_time = time.strftime("%Y-%m-%d_%HH%MM")
-    p_i: pipeline.BasePipeline = pipeline_implementation(f'{pipeline_name}_{results_current_time}', **pipeline_kwargs).add_train_data_source(*actual_used_training_data_files_paths)
-
+    p: pipeline.BasePipeline = pipeline_implementation(pipeline_name, **pipeline_kwargs).set_description(f'{pipeline_name}. Built on: {results_current_time}').add_train_data_source(*actual_used_training_data_files_paths)
     # Try to build
     try:
-        p_i = p_i.build(skip_accuracy_score=True)
+        p = p.build()
     except Exception as e:
-        info = f'PerpRaw={p_i._tsne_perplexity}/Perp={p_i.tsne_perplexity}/' \
-               f'EE={p_i.tsne_early_exaggeration}/LR={p_i.tsne_learning_rate}/GMM-N={p_i.gmm_n_components}'
+        info = f'PerpRaw={p._tsne_perplexity}/Perp={p.tsne_perplexity}/' \
+               f'EE={p.tsne_early_exaggeration}/LR={p.tsne_learning_rate}/GMM-N={p.gmm_n_components}'
         err = f'Unexpected exception::{__name__}.{logging_enhanced.get_current_function()}(): ' \
               f'an unexpected exception occurred when building many pipelines to get good graphs. ' \
               f'Info is as follows: {info}. Exception is: {repr(e)}.'  # Diagnostics: {p_i.diagnostics()}'
         logger.error(err)
         raise RuntimeError(err)
-    else:
-        # Get info
-        perplexity_ratio_i, perplexity_i, learning_rate_i, early_exaggeration_i = p_i.tsne_perplexity_relative_to_num_data_points, p_i.tsne_perplexity, p_i.tsne_learning_rate, p_i.tsne_early_exaggeration
 
-        graph_title = f"Perp ratio: {round(perplexity_ratio_i, 5)} / " \
-                      f"Perp: {perplexity_i} / EE: {early_exaggeration_i} / " \
-                      f"LearnRate: {learning_rate_i} / #data={p_i.num_training_data_points} / tsneNiter={p_i.tsne_n_iter}"
-        # Save graph to file
-        p_i.plot_clusters_by_assignments(
-            title=graph_title,
-            fig_file_prefix=f'standardscaler1{results_current_time}__{p_i.name}__',
-            show_now=False, save_to_file=True, figsize=graph_dimensions,
-            s=0.4 if show_cluster_graphs_in_a_popup_window else 1.5,
-        )
-        # p_i.save_to_folder()
-        # Try to make example vids
-        # p_i.make_behaviour_example_videos(vid_data_source, vid_file_path, file_name_prefix='timtest4', num_frames_buffer=1)  #     def make_behaviour_example_videos(self, data_source: str, video_file_path: str, file_name_prefix=None, min_rows_of_behaviour=1, max_examples=3, num_frames_buffer=0, output_fps=15):
+    ### If no exceptions raised, continue
+    # Save pipeline to folder
+    if save_to_folder:
+        p.save_to_folder(config.OUTPUT_PATH)
+
+    # Get info
+    perplexity_ratio_i, perplexity_i, learning_rate_i, early_exaggeration_i = p.tsne_perplexity_relative_to_num_data_points, p.tsne_perplexity, p.tsne_learning_rate, p.tsne_early_exaggeration
+
+    graph_title = f"Perp ratio: {round(perplexity_ratio_i, 5)} / " \
+                  f"Perp: {perplexity_i} / EE: {early_exaggeration_i} / " \
+                  f"LearnRate: {learning_rate_i} / #data={p.num_training_data_points} / tsneNiter={p.tsne_n_iter}"
+    # Save graph to file
+    p.plot_clusters_by_assignments(
+        title=graph_title,
+        fig_file_prefix=f'standardscaler1{results_current_time}__{p.name}__',
+        show_now=False, save_to_file=save_graph_to_file, figsize=graph_dimensions,
+        s=0.4 if show_cluster_graphs_in_a_popup_window else 1.5,
+    )
+    # p_i.save_to_folder()
+    # Try to make example vids
+    # p_i.make_behaviour_example_videos(vid_data_source, vid_file_path, file_name_prefix='timtest4', num_frames_buffer=1)  #     def make_behaviour_example_videos(self, data_source: str, video_file_path: str, file_name_prefix=None, min_rows_of_behaviour=1, max_examples=3, num_frames_buffer=0, output_fps=15):
 
     end_build = time.perf_counter()
     build_time_secs = round((end_build-start_build), 2)
-    logger.info(f'Time to build: {build_time_secs} seconds (using {max_cores_per_pipe} cores)')
+    logger.info(f'Time to build "{pipeline_name}": {build_time_secs} seconds (using {max_cores_per_pipe} cores)')
     logger.debug(f'Done job at: {time.strftime("%Y-%m-%d_%HH%MM")}')
 
 
