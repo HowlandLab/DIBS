921a2a7a773f889660a3975825825303b0951582
Aaron Toderash (adt150) <aaron.t@usask.ca>
2021-05-12 19:32:27 -0600

    Changed default view window for extraction of example videos

--- dibs/streamlit_app.py
+++ dibs/streamlit_app.py
@@ -965,7 +965,8 @@ def review_behaviours(p: pipeline.BasePipeline, pipeline_file_path):
         file_name_prefix = st.text_input(f'File name prefix. This prefix will help differentiate between example video sets.')
         number_input_output_fps = st.number_input(f'Output FPS for example videos', value=8., min_value=1., step=1., format='%.2f')
         number_input_max_examples_of_each_behaviour = st.number_input(f'Maximum number of videos created for each behaviour', value=5, min_value=1)
-        number_input_min_rows = st.number_input(f'Number of rows of data required for a detection to occur', value=1, min_value=1, max_value=100, step=1)
+        # TODO: AARON: Number min rows crashed the app when trying to adjust from streamlit.
+        number_input_min_rows = st.number_input(f'Number of rows of data required for a detection to occur', value=3, min_value=1, max_value=100, step=1)
         number_input_frames_leadup = st.number_input(f'Number of rows of data that precede and succeed target behaviour', value=5, min_value=0)
 
         st.markdown('')
