2651007674d8777a7c4a7114532701ed09da0a69
Aaron Toderash (adt150) <aaron.t@usask.ca>
2021-05-17 09:17:23 -0600

    transition matrix script

---
+++ custom_scripts/transition_matrix.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+import math as m
+
+df = pd.read_csv(sys.argv[1])
+n = len(df.gmm_assignment.unique()) # number of clusters
+
+mat = np.zeros((n, n)) # mat to be filled with assignment transition counts
+
+gmm_a = df.gmm_assignment
+
+for b1,b2 in zip(gmm_a[0:-2], gmm_a[1:-1]):
+    mat[b1,b2] += 1
+
+
+print(mat)
+log_mat = np.log(mat + 1)
+print(log_mat)
+norm_mat = mat / np.max(mat)
+print(norm_mat)
+
+int_mat_zero_diag = np.array(mat, dtype='int') # diagonal to zero
+np.fill_diagonal(int_mat_zero_diag, 0)
+
+# set seaborn figure size in inches
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+cmap=sns.color_palette("Blues", as_cmap=True)
+
+def heatmap(m, name, **kwargs):
+    plt.clf()
+    hm = sns.heatmap(m, cmap=cmap, **kwargs)
+    fig = hm.get_figure()
+    fig.savefig(f'{name}.png')
+
+
+
+## Commented these out because they were not terribly useful
+# heatmap(log_mat, 'log_mat')
+# heatmap(norm_mat, 'norm_mat') # looks weird
+
+# norm_zero_diag_mat = int_mat_zero_diag / np.max(int_mat_zero_diag)
+# print(norm_zero_diag_mat)
+
+# heatmap(norm_zero_diag_mat, 'norm_zero_diag_mat',
+#         annot=True, fmt='.2f')
+
+# This was the most useful
+heatmap(int_mat_zero_diag, 'mat',
+        annot=True, fmt='d')
+
+plt.show() # only last plot can be shown
+plt.clf()  # clear
+
+
