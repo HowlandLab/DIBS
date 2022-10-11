41ca0f2170609e610bd01fe077a1d3a4ab4ecb64
Aaron Toderash <toddy945@gmail.com>
2022-10-11 11:57:52 -0600

    Updates to input validation for csv scripts

--- hackathon_sept_2022/create_interaction_to_video_summary_csv.py
+++ hackathon_sept_2022/create_interaction_to_video_summary_csv.py
@@ -5,8 +5,16 @@ import pandas as pd
 
 # INSTRUCTIONS: 2 args.  <in directory with csvs to summarize> <out file name to place summary csv in>
 
+if len(sys.argv) != 3:
+    raise RuntimeError('There is an error with the input arguments.  Expect 2 arguments, 1st: Input directory with csvs to summaryize.  2nd: Path to an output file where the results will be stored, must not exist.  Try adding quotes around the input arguments (each individually)')
+
 indir=sys.argv[1]
 outfile=sys.argv[2]
+
+
+if os.path.exists(outfile):
+    raise RuntimeError('The outfile already exists!  We will not overwrite any files!  Please provide a path that does not already exist!')
+
 
 print(indir)
 
